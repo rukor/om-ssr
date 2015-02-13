10b5d986775b75585d31230027a1711a03c88c81
Roland Ukor <roland@firstlinq.com>
2015-02-13 23:44:37 +0000

    Add silk dependency to main dependencies.

--- project.clj
+++ project.clj
@@ -7,7 +7,9 @@
             :dependencies [[org.clojure/clojure "1.6.0"]
                            [org.clojure/clojurescript "0.0-2816" :scope "provided"]
                            [org.clojure/core.async "0.1.346.0-17112a-alpha"]
-                           [org.omcljs/om "0.8.8" :scope "provided"]
+                           [org.omcljs/om
+                            "0.8.8" :scope "provided"]
+                           [com.domkm/silk "0.0.4"]
                            [com.taoensso/timbre "3.3.1"]
                            [com.cognitect/transit-clj "0.8.259"]
                            [com.cognitect/transit-cljs "0.8.205"]]
@@ -19,5 +21,4 @@
                              :rules        :clj}
                             {:source-paths ["src"]
                              :output-path  "target/src"
-                             :rules        :cljs}]}
-            :profiles {:dev {:dependencies [[com.domkm/silk "0.0.4"]]}})
+                             :rules        :cljs}]})
