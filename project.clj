e131a770f90309e9853974650f3b00a5b896b9c1
Roland Ukor <roland@firstlinq.com>
2015-04-15 14:25:12 +0100

    Back to 0.0.4

--- project.clj
+++ project.clj
@@ -9,7 +9,7 @@
                            [org.clojure/core.async "0.1.346.0-17112a-alpha"]
                            [org.omcljs/om "0.8.8" :scope "provided"]
                            [prismatic/om-tools "0.3.10"]
-                           [com.domkm/silk "0.0.5"]
+                           [com.domkm/silk "0.0.4"]
                            [bidi "1.18.7"]
                            [com.taoensso/timbre "3.3.1"]
                            [com.cognitect/transit-clj "0.8.259"]
